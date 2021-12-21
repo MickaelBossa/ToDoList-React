0d29f2cbf8a86fa4e866d078050b7bfa737ecdcb
Mickael Bossa <mickael.bossa@outlook.com>
2021-12-21 21:51:52 +0100

    Adding css modules and styled components

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,16 +1,38 @@
 import Item from "./Item";
 import { useState } from "react";
+import styled from 'styled-components';
+import styles from './Form.module.css'
+
+const AppForm = styled.form`
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+`;
+
+const TextInput = styled.input`
+  display: block;
+  width: 80%;
+  margin-top: 1rem;
+`;
+
+const SubmitBtn = styled.button`
+  background-color: blue;
+  color: white;
+  border-radius: 0.1rem;
+  height: 2rem;
+  margin-top: 1rem;
+`;
 
 export default function Form() {
   return (
     <div>
-      <form className="form">
+      <AppForm>
         <label htmlFor="todo">Chose à faire</label>
-        <input type="text" className="input" />
-        <button className="submitBtn">Envoyez</button>
-      </form>
+        <TextInput type="text"/>
+        <SubmitBtn>Envoyez</SubmitBtn>
+      </AppForm>
 
-      <h2 className="subtitle">Liste des choses à faire : </h2>
+      <h2 className={styles.subtitle}>Liste des choses à faire : </h2>
       <ul>
         <Item />
       </ul>
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,8 +1,32 @@
+import styled from 'styled-components';
+import styles from './Item.module.css';
+
+const List = styled.li`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 0.1rem black solid;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+
+
+const DeleteBtn = styled.button`
+  background-color: crimson;
+  color: white;
+  height: 2rem;
+  border-radius: 0.1rem;
+  margin-right: 1rem;
+`;
+
 export default function Item() {
   return (
-    <li className="list">
-      <p className="txt">Test</p>
-      <button className="deleteBtn">Supprimer</button>
-    </li>
+    <List>
+      <p className={styles.textContent}>Test</p>
+      <DeleteBtn>Supprimer</DeleteBtn>
+    </List>
   );
 }
