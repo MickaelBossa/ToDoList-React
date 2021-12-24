0a8b5d725128f4fbc0db4e1e52a7a4cde73b80fc
BOSSA Mickael <mickael.bossa@outlook.com>
2021-12-24 18:01:21 +0100

    Adding the logic

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,80 +1,72 @@
-import React from 'react';
+import {useState, useRef} from 'react';
 import './App.css';
+import Item from '../../Components/Item';
 
 function App() {
+
+// --------------------STATES----------------------------------
+  const [tasks, setTasks] = useState([
+    {
+      txt: 'Apprendre React',
+
+    },
+    {
+      txt: 'Faire du sport',
+    },
+  ]);
+
+  const inputRef = useRef();
+
+// --------------------METHODS----------------------------------
+  const removedClickHandler = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index,1);
+    setTasks(newTasks);
+  }
+
+  let cards = tasks.map((task, index) => {
+    return(
+      <Item 
+      txt = {task.txt}
+      deleteTask = {() => removedClickHandler(index)}
+      key={index}
+      />
+    )
+  })
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const newArr = [...tasks];
+    const newTodo = {};
+    newTodo.txt = e.target[0].value;
+    newArr.push(newTodo);
+    setTasks(newArr);
+    inputRef.current.value="";
+  }
+
+  // --------------------RETURN----------------------------------
   return (
     <div className="App">
+{/*-------------------- TITLE --------------------------*/}
       <header>
         <span>TO-DO</span>
       </header>
 
+{/*---------------------- FORM ----------------------------*/}
       <div className="add">
-        <form>
-          <input type="text" placeholder="Que souhaitez-vous ajouter ?" />
+        <form
+        onSubmit={(e) => addTask(e)}
+        >
+          <input type="text" placeholder="Que souhaitez-vous ajouter ?" ref={inputRef}/>
           <button type="submit">
             Ajouter
           </button>
         </form>
       </div>
 
-      <div className="task">
-        <div className="content">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-          </svg>
-          Aller chercher les courses
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      </div>
-
-      <div className="task">
-        <div className="content">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
-          </svg>
-          <strike>
-            Aller chercher les courses
-          </strike>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      </div>
-      
-      <div className="task">
-        <div className="content">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
-          </svg>
-          <strike>
-            Aller chercher les courses
-          </strike>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      </div>
-
-      <div className="task">
-        <div className="content">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
-          </svg>
-          <strike>
-            Aller chercher les courses
-          </strike>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      </div>
-
-    </div>
+{/*------------------------------ TASKS --------------------------*/}
+    {cards}
+  </div>
   );
 }
 
